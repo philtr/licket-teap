9d9b751a37ea951c3a061c0971e79d43c818268e
Phillip Ridlen <phillip@balcomagency.com>
2012-01-20 16:44:42 -0600

    Require instead of autoload

--- lib/licket_teap.rb
+++ lib/licket_teap.rb
@@ -2,13 +2,12 @@ require 'bundler/setup'
 Bundler.require
 
 $:.unshift File.dirname(__FILE__)
+require "licket_teap/base.rb"
+require "licket_teap/organization.rb"
+require "licket_teap/event.rb"
+require "licket_teap/performance.rb"
 
 module LicketTeap
-  autoload :Base,         "licket_teap/base.rb"
-  autoload :Organization, "licket_teap/organization.rb"
-  autoload :Event,        "licket_teap/event.rb"
-  autoload :Performance,  "licket_teap/performance.rb"
-
   def self.new(api_key)
     Base.new(api_key)
   end
